1786ca5361bdef86be55d12de3f5d15ca83123a4
Aayush kumar <aayush.21jdai066@jietjodhpur.ac.in>
2024-03-21 12:13:12 +0530

    Added authentication system using Clerk and set up sidebar structure

---
+++ src/app/(root)/layout.tsx
@@ -0,0 +1,17 @@
+import Sidebar from "@/components/shared/Sidebar";
+import React from "react";
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <main className="root">
+      {/* sidebar */}
+      <Sidebar />
+      {/* MobileNav */}
+      <div className="root-container">
+        <div className="wrapper">{children}</div>
+      </div>
+    </main>
+  );
+};
+
+export default Layout;
