f1ee11cd49a7efe6ded2eac3659e809d9aac08b6
Aayush kumar <aayush.21jdai066@jietjodhpur.ac.in>
2024-03-23 11:27:40 +0530

    Successfully connected MongoDB database and created models for user, image, and transaction in the Next.js web app.

---
+++ src/lib/database/models/user.model.ts
@@ -0,0 +1,55 @@
+import { Document, Schema, model, models } from "mongoose";
+
+
+// imediately create a type for this image because i am working with typescript
+export interface User extends Document {
+  clerkId: string;
+  email: string;
+  username: string;
+  photo: string;
+  firstName?: string;
+  lastName?: string;
+  planId?: number;
+  creditBalance?: number;
+}
+
+const UserSchema = new Schema({
+  clerkId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  photo: {
+    type: String,
+    required: true,
+  },
+  firstName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+  },
+  planId: {
+    type: Number,
+    default: 1,
+  },
+  creditBalance: {
+    type: Number,
+    default: 10,
+  },
+});
+
+const User = models?.User || model("User", UserSchema);
+
+export default User;
+
