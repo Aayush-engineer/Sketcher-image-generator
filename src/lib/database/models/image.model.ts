6b04a9348f9576c525a7efaaa9582c607507b0cb
Aayush kumar <aayush.21jdai066@jietjodhpur.ac.in>
2025-09-03 00:36:50 +0530

    feat(profile, db, ui): update profile page & image model, add Collection, Search, and Pagination components

--- src/lib/database/models/image.model.ts
+++ src/lib/database/models/image.model.ts
@@ -1,45 +1,42 @@
 import { Document, Schema, model, models } from "mongoose";
 
-// imediately create a type for this image because i am working with typescript
-export interface Image extends Document {
+export interface IImage extends Document {
   title: string;
   transformationType: string;
   publicId: string;
-  secureUrl: string;
+  secureURL: string; 
   width?: number;
   height?: number;
-  config?: object;
-  transformationUrl?: string;
+  config?: object; 
+  transformationUrl?: string; 
   aspectRatio?: string;
   color?: string;
   prompt?: string;
-  author?: {
+  author: {
     _id: string;
     firstName: string;
     lastName: string;
-  };
+  }
   createdAt?: Date;
   updatedAt?: Date;
 }
-
-
 
 const ImageSchema = new Schema({
   title: { type: String, required: true },
   transformationType: { type: String, required: true },
   publicId: { type: String, required: true },
-  secureUrl: { type: URL, required: true },
+  secureURL: { type: String, required: true },
   width: { type: Number },
   height: { type: Number },
   config: { type: Object },
-  transformationUrl: { type: URL },
+  transformationUrl: { type: String },
   aspectRatio: { type: String },
   color: { type: String },
   prompt: { type: String },
   author: { type: Schema.Types.ObjectId, ref: 'User' },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now }
-})
+});
 
 const Image = models?.Image || model('Image', ImageSchema);
 
